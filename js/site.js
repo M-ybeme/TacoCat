f52489b332bd8a3260efcdfe5d31a1b5a28d55b9
M-ybeme <marlomayberry.90@gmail.com>
2022-07-19 17:05:20 -0400

    final toutches

--- js/site.js
+++ js/site.js
@@ -1,17 +1,42 @@
-function getValue(message){
-    alert("Hello World!");
+//control function
+function getValue() {
+    let message = document.getElementById("message").value;
+    let revMessage = reverse(message);
+    displayMessage(message, revMessage);
 }
 
-function getValue2()
-{
-    let msg = '';
-    msg = document.getElementById("message").value;
 
-    swal.fire(
-    {
-        backdrop: false,
-        title: 'APP NAME',
-        text: msg
-        }
-    )
+//buisness logic
+function reverse(message) {
+
+    //string an array of characters  with index and values
+    let reversedMesssage = "";
+
+    //decrementing for loop
+    for (let index = message.length-1; index >= 0; index--) {
+
+        reversedMesssage += message[index];
+    }
+    return reversedMesssage;
+}
+
+function displayMessage(message, revMessage) {
+
+    let div= document.createElement("div")
+    
+    let newMessage = message.replace(/\s/g, "");
+    let newRevMessage = revMessage.replace(/\s/g, "");
+
+        if(newMessage.toLowerCase() === newRevMessage.toLowerCase()){
+        
+            results.innerHTML = "Yes, This is a palindrome.";
+            results.classList.add("success");
+        
+        } else {
+        
+            results.innerHTML = "No, This is not a palindrome.";
+            results.classList.add("fail");
+    }
+    results.appendChild(div);
+        
 }
